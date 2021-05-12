c6e74672ccaa05f8dcd0a9a5dd64b39640cd101a
Christian Lee <christian.r.lee@gmail.com>
2021-05-12 19:28:50 +0000

    Create histogram.m

---
+++ histogram.m
@@ -0,0 +1,8 @@
+figure
+histogram(imported_data.data(:,1));
+
+
+histogram(imported_data.data(:,1),'Normalization','cdf')
+
+
+% also https://www.mathworks.com/matlabcentral/answers/426214-how-to-plot-a-histogram-as-a-curve
