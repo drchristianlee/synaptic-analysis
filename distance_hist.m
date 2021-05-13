1ae24884c8d18399d0beea01d928e0d015401cff
Christian Lee <christian.r.lee@gmail.com>
2021-05-13 17:08:05 +0000

    Update distance_hist.m

--- distance_hist.m
+++ distance_hist.m
@@ -2,12 +2,40 @@
 cd(path);
 load(file_nm);
 
-figure
+plot_select = str2num(cell2mat(inputdlg('Would you like to plot a cdf histogram ? Press 1 for yes')));
 
+figure
 histogram(imported_data.data(:,1));
+
+figure
+histogram(imported_data.data(:,1), 'Normalization', 'probability')
 
 figure
 histogram(imported_data.data(:,1),'Normalization','cdf')
 
+if plot_select == 1;
+    
+[values, edges] = histcounts(imported_data.data(:,1), 'Normalization', 'cdf');
+centers = (edges(1:end-1)+edges(2:end))/2;
+figure
+plot(centers, values, 'k-')
+% Plot both, line superimposed on the histogram
+figure
+h = histogram(imported_data.data(:,1), 'Normalization', 'cdf');
+hold on
+plot(centers, values, 'k-')
 
-% also https://www.mathworks.com/matlabcentral/answers/426214-how-to-plot-a-histogram-as-a-curve
+else
+    
+[values, edges] = histcounts(imported_data.data(:,1), 'Normalization', 'probability');
+centers = (edges(1:end-1)+edges(2:end))/2;
+figure
+plot(centers, values, 'k-')
+% Plot both, line superimposed on the histogram
+figure
+h = histogram(imported_data.data(:,1), 'Normalization', 'probability');
+hold on
+plot(centers, values, 'k-')
+
+end
+% https://www.mathworks.com/matlabcentral/answers/426214-how-to-plot-a-histogram-as-a-curve
