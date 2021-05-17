d59af003093f40d728bba858d3eb44ae06c6d072
Christian Lee <christian.r.lee@gmail.com>
2021-05-17 20:13:52 +0000

    Update distance_hist.m

--- distance_hist.m
+++ distance_hist.m
@@ -6,29 +6,36 @@ load(file_nm);
 
 plot_select = str2num(cell2mat(inputdlg('Would you like to plot a cdf histogram ? Press 1 for yes')));
 
+figure(1)
+figure(2)
+
 if plot_select == 1;
-    
-[values, edges] = histcounts(imported_data.data(:,1), 'Normalization', 'cdf');
-centers = (edges(1:end-1)+edges(2:end))/2;
-figure
-plot(centers, values, 'k-')
-% Plot both, line superimposed on the histogram
-figure
-h = histogram(imported_data.data(:,1), 'Normalization', 'cdf');
-hold on
-plot(centers, values, 'k-')
-
+    for stepper = 1:size(imported_data.data, 2);
+    [values, edges] = histcounts(imported_data.data(:, stepper), 'Normalization', 'cdf');
+    centers = (edges(1:end-1)+edges(2:end))/2;
+    figure(1)
+    plot(centers, values, 'k-')
+    hold on
+    % Plot both, line superimposed on the histogram
+    figure(2)
+    h = histogram(imported_data.data(:, stepper), 'Normalization', 'cdf');
+    hold on
+    plot(centers, values, 'k-')
+    hold on
+    end
 else
-    
-[values, edges] = histcounts(imported_data.data(:,1), 'Normalization', 'probability');
-centers = (edges(1:end-1)+edges(2:end))/2;
-figure
-plot(centers, values, 'k-')
-% Plot both, line superimposed on the histogram
-figure
-h = histogram(imported_data.data(:,1), 'Normalization', 'probability');
-hold on
-plot(centers, values, 'k-')
-
+    for stepper = 1:size(imported_data.data, 2);
+    [values, edges] = histcounts(imported_data.data(:, stepper), 'Normalization', 'probability');
+    centers = (edges(1:end-1)+edges(2:end))/2;
+    figure(1)
+    plot(centers, values, 'k-')
+    hold on
+    % Plot both, line superimposed on the histogram
+    figure(2)
+    h = histogram(imported_data.data(:, stepper), 'Normalization', 'probability');
+    hold on
+    plot(centers, values, 'k-')
+    hold on
+    end
 end
 % https://www.mathworks.com/matlabcentral/answers/426214-how-to-plot-a-histogram-as-a-curve
